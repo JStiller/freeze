996607dbfea7d9ac86266caf7ac21a12b516315c
José Stiller <info@josestiller.de>
2017-08-06 09:58:27 +0100

    added first implementation

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # freeze
+Freezes the body node, triggered by the delivered node
+
+This is useful if you want to scroll in an popup which is layered on the body.
+This should prevent that the user is able to scroll both contents.
+
+```js
+var myFreezeInstance = new freeze();
+
+myFreezeInstance.element(document.querySelector('...'));
+```
---
+++ freeze.js
@@ -0,0 +1,31 @@
+var freeze = (function (dependency) {
+  var nodeBody = dependency.document.querySelector('body');
+
+  /**
+   * freezes the body node, triggered by the delivered node
+   * 
+   * @param {object} deliveredNode
+   * @return {object}
+   */
+  function element(deliveredNode) {
+    if(deliveredNode.nodeName === 'INPUT' && deliveredNode.getAttribute('type') === 'checkbox') {
+      deliveredNode.addEventListener('change', function() {
+        if(typeof nodeBody.style.overflow === 'undefined') {
+          nodeBody.style.overflow = "hidden";
+        } else {
+          nodeBody.style.overflow = nodeBody.style.overflow === 'hidden' ? 'inherit' : 'hidden';
+        }
+      }, false);
+    } else {
+      throw new TypeError();
+    }
+
+    return this;
+  }
+
+  return {
+    element: element
+  }
+}({
+  document: document,
+}));
